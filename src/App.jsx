4e75a60f0429b3fb1c47faa51e9cb0d7d88de997
christian <christianduculannn@gmail.com>
2025-11-23 01:31:53 +0800

    env

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./LandingPage/Login";
 import Dashboard from "./DriverModule/Dashboard";
 import ShopModule from "./ShopModule/ShopModule";
-import AdminModule from "./AdminModule/AdminModule";
+import AdminModule from "./AdminModule/AdminModule"; // keep it here
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} />       {/* Landing page */}
         <Route path="/driver" element={<Dashboard />} />
         <Route path="/shop" element={<ShopModule />} />
+        <Route path="/admin" element={<AdminModule />} /> {/* Only loads here */}
+        <Route path="*" element={<Login />} />      {/* SPA fallback */}
       </Routes>
     </BrowserRouter>
   );
